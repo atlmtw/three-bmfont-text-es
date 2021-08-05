f6b718cff22981e6ba684c49901e5d901d2bbb4c
atlmtw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 <masontywong@gmail.com>
2021-08-05 01:00:16 -0400

    revert

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var createIndices = require('quad-indices')
 var vertices = require('./lib/vertices')
 var utils = require('./lib/utils')
 
-var Base = new THREE.BufferGeometry
+var Base = THREE.BufferGeometry
 
 module.exports = function createTextGeometry (opt) {
   return new TextGeometry(opt)
