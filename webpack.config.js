8094ac7793f4170017f2d4f818cc98517031ded7
atlmtw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 <masontywong@gmail.com>
2022-07-19 13:26:24 -0400

    change peer dependency line

--- webpack.config.js
+++ webpack.config.js
@@ -23,17 +23,17 @@ module.exports = {
         hot: true,
         compress: true,
         historyApiFallback: true,
-        module: {
-            rules: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
-            }, ]
-        }
     },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                },
+            },
+        }, ]
+    }
 };
