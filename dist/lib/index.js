89f09011018e4622e26bcf9ff314424c052468f5
atlmtw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 <masontywong@gmail.com>
2022-07-19 13:35:53 -0400

    use babel build test

---
+++ dist/lib/index.js
@@ -0,0 +1,4 @@
+import { createTextGeometry } from "./createTextGeometry";
+import { computeBox, computeSphere } from "./utils";
+import { pages, uvs, positions } from "./vertices";
+export { createTextGeometry, computeBox, computeSphere, pages, uvs, positions };
---
+++ dist/shaders/index.js
@@ -0,0 +1,5 @@
+import { createBasicShader } from "./basic";
+import { createMSDFShader } from "./msdf";
+import { createMultipageShader } from "./multipage";
+import { createSDFShader } from "./sdf";
+export { createBasicShader, createMSDFShader, createMultipageShader, createSDFShader };
